2a6d4f064cc7434fe97074cad71cbbe74124cb9e
Devin Riegle <devin.riegle@joinroot.com>
2019-05-12 22:17:27 -0400

    export account & items

--- lib/plaid_dart.dart
+++ lib/plaid_dart.dart
@@ -4,5 +4,7 @@
 library plaid_dart;
 
 export 'src/plaid_dart_base.dart';
+export 'src/models/account.dart';
+export 'src/models/item.dart';
 
 // TODO: Export any libraries intended for clients of this package.
